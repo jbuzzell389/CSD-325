79f0a1f165e9a18cbc0c3a4c7ace568495f293b0
jbuzzell389 <890004953@ciat.edu>
2025-12-14 19:14:12 -0600

    pushed 12/14/2025

---
+++ module-11/jbuzzelldjango/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+
+# Create your views here.
+
+def congrats(request):
+    return HttpResponse("Buzzell says hello! Congratulations!")
+
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
